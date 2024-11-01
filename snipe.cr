5adf89981e6ac240243644e7f90369a710dcb902
OpenSauce04 <opensauce04@gmail.com>
2024-11-01 15:58:19 +0000

    Implemented simple file search

--- snipe.cr
+++ snipe.cr
@@ -1,1 +1,9 @@
-puts "Hello world"
+files = Dir["**/*"]
+
+loop do
+	search = gets || ""
+	next if search.empty?
+
+	matched_files = files.select(/#{Regex.escape(search)}[^\/]*$/)
+	puts matched_files
+end
